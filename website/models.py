a835bfc87f5890b9e3e88dc8b919e6c4ed690f04
Melvin Millayes <melvin_millayes@yahoo.com>
2024-04-15 22:21:22 -0400

    Create post functionality

--- website/models.py
+++ website/models.py
@@ -1,12 +1,20 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy import func
 
-class user(db.model):
-  id = db.column(db.Integer, primary_key = True)
-  email = db.column(db.String(115), unique= True)
-  password =  db.column(db.String(115))
-  first_name = db.column(db.String(115))
-  last_name =  db.column(db.String(115))
+class News(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  data = db.Column(db.String(10000))
+  date = db.Column(db.DateTime(timezone = True), default = func.now())
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key = True)
+  email = db.Column(db.String(115), unique= True)
+  password = db.Column(db.String(115))
+  first_name = db.Column(db.String(115))
+  last_name = db.Column(db.String(115))
+  post = db.relationship('News')
 
 
 
--- website/views.py
+++ website/views.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, flash , request
+from flask_login import login_required, current_user
+from .models import News, User
+from . import db
 views = Blueprint('views', __name__)
 
-@views.route("/")
+
+
+@views.route("/", methods = ["GET", "POST"])
+@login_required
 def home():
-  return render_template("home.html")
+  if request.method == "POST":
+    news = request.form.get('news')
+
+    if len(news) < 1:
+      flash("Post is too short", category='error')
+    else:
+      new_post = News(data = news, user_id = current_user.id)
+      flash("Post added succesfully", category='success')
+      db.session.add(new_post)
+
+      db.session.commit()
+  return render_template("home.html", user = current_user)
